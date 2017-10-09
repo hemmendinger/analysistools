f3eca71d94cf887bb906f2a2f6a6395e4e8c8e7b
Hemmendinger <github@hemmendinger.net>
2017-10-09 08:33:36 -0400

    added startup.py, provides imports for iPython shell

---
+++ startup.py
@@ -0,0 +1,10 @@
+import pyarrow.parquet as pq
+import pyarrow as pa
+
+import numpy as np
+import pandas as pd
+
+#from scipy import stats, integrate
+
+import matplotlib.pyplot as plt
+import seaborn as sns
